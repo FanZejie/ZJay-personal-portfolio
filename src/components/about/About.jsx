9ba64aadb77f506d5b6c13ad623cfdef6d78858c
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-06-05 17:51:37 +0800

    更换个人简历

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/zjayFan.JPG";
-import CV from "../../assets/Smith-Cv.pdf";
+import CV from "../../assets/zejieFan-CV.pdf";
 import Info from "./Info";
 
 const About = () => {
