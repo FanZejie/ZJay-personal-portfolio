9d181483b2559585ba4c9630062889e5c9f5fa21
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-06-05 17:49:14 +0800

    更换个人简历

--- src/components/Portfolio/WorkItems.jsx
+++ src/components/Portfolio/WorkItems.jsx
@@ -5,10 +5,19 @@ const WorkItems = ({ item }) => {
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="" className="work__img" />
       <h3 className="work__title">{item.title}</h3>
-      <a href="#" className="work__button">
+      <div className="link__box">
+      <div  className="work__button" onClick={() => alert('Demo coming soon')}>
         Demo
         <i className="bx bx-right-arrow-alt work__button-icon"></i>
-      </a>
+      </div>
+
+      <div  className="work__button" onClick={() => alert('github coming soon')}>
+        github
+        <i className="bx bx-right-arrow-alt work__button-icon"></i>
+      </div>
+      </div>
+      
+      
     </div>
   );
 };
